b6117fca7e00ec1fe1825908a91f551623359a08
GuptaDivi3007 <gdivya3007@gmail.com>
2025-12-18 12:05:04 +0530

    [Divyanshi Gupta] Area Of Circle

---
+++ core-java-practice/gcr-codebase/programming-elements/areaOfCircle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class areaOfCircle {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double r = sc.nextDouble();
+        System.out.println(3.14 * r * r);
+	}
+}
