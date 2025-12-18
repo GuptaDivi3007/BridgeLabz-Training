6a36319a2234b133d2cdd5497b4d121e148da780
GuptaDivi3007 <gdivya3007@gmail.com>
2025-12-18 12:06:08 +0530

    [Divyanshi Gupta] Calculate SI

---
+++ core-java-practice/gcr-codebase/programming-elements/calculateSI.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class calculateSI {
+
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double p = sc.nextDouble();
+        double r = sc.nextDouble();
+        double t = sc.nextDouble();
+
+        double SI = (p * r * t) / 100;
+        System.out.println(SI);
+    }
+}
