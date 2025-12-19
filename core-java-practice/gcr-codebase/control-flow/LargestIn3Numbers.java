b5e172187b903fc63959255fffab5acda922377f
GuptaDivi3007 <gdivya3007@gmail.com>
2025-12-19 16:01:48 +0530

    [Divyanshi Gupta] Add.Added level1 programs

---
+++ core-java-practice/gcr-codebase/control-flow/LargestIn3Numbers.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class LargestIn3Numbers {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int n3 = sc.nextInt();
+
+        if (n1 > n2 && n1 > n3) {
+            System.out.println("Is the first number the largest? Yes");
+        }
+        else {
+            System.out.println("Is the first number the largest? No");
+        }
+
+        if (n2 > n1 && n2 > n3) {
+            System.out.println("Is the second number the largest? Yes");
+        }
+        else {
+            System.out.println("Is the second number the largest? No");
+        }
+
+        if (n3 > n1 && n3 > n2) {
+            System.out.println("Is the third number the largest? Yes");
+        }
+        else {
+            System.out.println("Is the third number the largest? No");
+        }
+    }
+}
