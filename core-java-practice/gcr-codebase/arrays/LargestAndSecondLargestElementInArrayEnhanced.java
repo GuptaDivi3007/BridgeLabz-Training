0401a800d2953eee238543c0784a87b56822c62d
GuptaDivi3007 <122393384+GuptaDivi3007@users.noreply.github.com>
2025-12-20 23:32:03 +0530

    Update class name

--- core-java-practice/gcr-codebase/arrays/LargestAndSecondLargestElementInArrayEnhanced.java
+++ core-java-practice/gcr-codebase/arrays/LargestAndSecondLargestElementInArrayEnhanced.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class LargestAndSecondLargestElementInArray {
+public class LargestAndSecondLargestElementInArrayEnhanced {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
