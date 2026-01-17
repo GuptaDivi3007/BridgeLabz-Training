b9397ff112ff0b135a15ca3085a87ad80b5e9721
GuptaDivi3007 <gdivya3007@gmail.com>
2026-01-17 23:36:00 +0530

    [Divyanshi Gupta] Add.Added filereader-inputstreamreader problems

---
+++ dsa-practice/gcr-codebase/filereader-inputstreamreader/ReadFileLineByLine.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFileLineByLine {
+
+    public static void main(String[] args) {
+
+        String filePath = "sample.txt"; // path to your text file
+
+        try (BufferedReader rd = new BufferedReader(new FileReader(filePath))) {
+
+            String line;
+            while ((line = rd.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        }
+        catch (IOException e) {
+            System.out.println("Error reading the file: " + e.getMessage());
+        }
+    }
+}
