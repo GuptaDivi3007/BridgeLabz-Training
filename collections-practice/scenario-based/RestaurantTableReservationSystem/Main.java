f9b710d9565493868ea585517369ebf6f6db778d
GuptaDivi3007 <gdivya3007@gmail.com>
2026-01-28 23:05:41 +0530

    [Divyanshi Gupta] Add.Added scenario based problems

---
+++ collections-practice/scenario-based/RestaurantTableReservationSystem/Main.java
@@ -0,0 +1,26 @@
+import java.time.LocalDateTime;
+public class Main {
+
+	public static void main(String[] args) {
+
+		ReservationSystem system = new ReservationSystem();
+
+        LocalDateTime slot = LocalDateTime.of(2026, 1, 28, 19, 0);
+
+        system.showAvailableTables(slot);
+
+        try {
+            system.reserveTable(1, "Khushi", slot);
+            system.reserveTable(1, "Riya", slot); // should throw exception
+        }
+        catch (TableAlreadyReservedException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        system.showAvailableTables(slot);
+
+        system.cancelReservation(1, slot);
+
+        system.showAvailableTables(slot);
+	}
+}
