9bfe57fcb8f6ee574edc73d48e3f64305ca3fb37
GuptaDivi3007 <gdivya3007@gmail.com>
2026-07-31 23:36:14 +0530

    [Divyanshi] initial commit DBMS fundamentals

---
+++ Day-1/CreateDatabase.sql
@@ -0,0 +1,11 @@
+-- Create database
+Create database health_clinic_db;
+
+-- Select database
+use health_clinic_db;
+
+-- View database
+Show databases;
+
+-- Dropping a database (careful — irreversible):
+drop database health_clinic_db;
---
+++ Day-1/CreateTable.sql
@@ -0,0 +1,14 @@
+
+-- Create table
+create table patients(
+	patient_id int auto_increment primary key,
+	first_name varchar(30) not null,
+	last_name varchar(30) not null,
+	date_of_birth date,
+	gender enum('Male', 'Female', 'Other'),
+	phone_number varchar(15) unique,
+	email varchar(100),
+	registered_on timestamp default current_timestamp
+);
+
+Select * from patients;
