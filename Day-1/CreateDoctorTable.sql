586100245d14a5289294fa3409d370a71839c92b
GuptaDivi3007 <gdivya3007@gmail.com>
2026-07-31 23:48:23 +0530

    [Divyanshi] initial commit DBMS fundamentals

---
+++ Day-1/CreateDoctorTable.sql
@@ -0,0 +1,18 @@
+-- Create Doctors table
+CREATE TABLE doctors (
+ doctor_id INT AUTO_INCREMENT PRIMARY KEY,
+ first_name VARCHAR(30) NOT NULL,
+ last_name VARCHAR(30) NOT NULL,
+ specialization VARCHAR(100),
+ phone_number VARCHAR(15) UNIQUE,
+ email VARCHAR(100)
+);
+
+-- Insert data
+INSERT INTO doctors (first_name, last_name, specialization, phone_number, email)
+VALUES
+('Anjali', 'Rao', 'Cardiology', '9123456780', 'dr.rao@clinic.com'),
+('Vikram', 'Iyer', 'Pediatrics', '9123456781', 'dr.iyer@clinic.com');
+
+-- Verify
+SELECT * FROM doctors;
