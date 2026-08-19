c73cbd1a1d8959fef40f9d857884a70f78252eae
GuptaDivi3007 <gdivya3007@gmail.com>
2026-08-20 00:50:37 +0530

    [Divyanshi] Add.Added built Employee Payroll App and tested using Postman and done junit testing

---
+++ Day-12/EmployeePayrollApp/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO employees (name, department, salary)
+VALUES
+('Divyanshi Gupta', 'Intern','20000.00'),
+('Muskan Gupta', 'Chemical Engineer','200000.00'),
+('Bandana Gupta', 'Teacher','216000.00');
