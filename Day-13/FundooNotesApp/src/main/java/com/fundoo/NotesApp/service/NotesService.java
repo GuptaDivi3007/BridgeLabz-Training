6ec7e1c344cfa31ba87f41cfbb8cae2fee5ffc9c
GuptaDivi3007 <gdivya3007@gmail.com>
2026-08-22 22:52:43 +0530

    [Divyanshi] Add.Added Bootstrapped Fundoo Notes App and is running successfully

---
+++ Day-13/FundooNotesApp/src/main/java/com/fundoo/NotesApp/service/NotesService.java
@@ -0,0 +1,11 @@
+package com.fundoo.NotesApp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotesService {
+
+    public String isRunning(){
+        return "app running successfully";
+    }
+}
